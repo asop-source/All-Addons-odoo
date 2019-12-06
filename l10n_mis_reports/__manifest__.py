175170876bb0bd9d12e003e6c47fecafa1f3c497
asop <asopkawang@mail.com>
2019-12-06 11:16:58 +0700

    update Balance

--- l10n_mis_reports/__manifest__.py
+++ l10n_mis_reports/__manifest__.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "vit_so_status",
+    'name': "Indonesian Profit Loss and Balance Sheet Reports for MIS Builder",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Report Profit and Loss
+        report Balance Sheet
+        """,
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Asop <vitraining.com>",
+    'website': "http://www.vitraining.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -20,13 +21,21 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale',],
+    'depends': ['base','mis_builder','vit_mis_kisel'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        # 'data_profit/mis.report.style.csv',
+        'data_profit/mis.report.csv',
+        'data_profit/mis.report.kpi.csv',
+        'data_profit/mis.report.instance.csv',
+        # 'data_balance/mis.report.style.csv',
+        'data_balance/mis.report.csv',
+        'data_balance/mis.report.kpi.csv',
+        'data_balance/mis.report.instance.csv'
     ],
     # only loaded in demonstration mode
     'demo': [
